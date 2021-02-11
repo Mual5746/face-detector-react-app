00070d07134eb34bad17dd4ac4f37d4477dd1499
mual5746 <megamax@MacBook-Air-som-tillhor-Max.local>
2021-02-11 17:10:14 +0100

    udated the face dection

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+       <div >                    
+         <div className = 'white f3'>
+          {'Din rank är ...'}   
+         </div>
+         <div className = 'white f1'>
+          {'#5'}   
+         </div>
+       </div>
+
+    );
+}
+
+export default Rank; 

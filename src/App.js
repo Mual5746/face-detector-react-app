9115a6d919c676f9f2a6ece85bd5be3b4c8224e4
Max Ali <mohseni716015@gmail.com>
2021-02-17 11:15:58 +0100

    new hchanges

--- src/App.js
+++ src/App.js
@@ -27,43 +27,72 @@ const particlesOptions = {
   }
 }
 
-class App extends Component  {
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      // https://i.pinimg.com/236x/cb/8a/cc/cb8accb4955dabd87a4bfc8a6435773c.jpg
-      input: '', // användarens input
-      imageUrl: '' 
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
-    //console.log(event.target.value)    
     this.setState({input: event.target.value});
   }
+
   onButtonSubmit = () => {
-    //console.log ('click')
     this.setState({imageUrl: this.state.input});
-
-     // https://i.pinimg.com/236x/cb/8a/cc/cb8accb4955dabd87a4bfc8a6435773c.jpg
-      
     app.models
-    
       .predict(
+        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+        // so you would change from:
+        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        // to:
+        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
         Clarifai.FACE_DETECT_MODEL,
-        //'https://i.pinimg.com/236x/cb/8a/cc/cb8accb4955dabd87a4bfc8a6435773c.jpg')
         this.state.input)
-       // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        //console.log(response)
-      },
-      function(err){
-
-      }
-  );
-      /*.then(response => {
+      .then(response => {
         console.log('hi', response)
         if (response) {
           fetch('http://localhost:3000/image', {
@@ -82,28 +111,47 @@ class App extends Component  {
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
       .catch(err => console.log(err));
-      */
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
 
-
-  render () {
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
-    <div className="App"> 
-        < Particles className = 'particles'
-        params ={particlesOptions} />      
-      < Navigation />
-      < Logo />
-      < Rank />
-      < ImageLinkForm 
-       onInputChange = {this.onInputChange} 
-       onButtonSubmit = {this.onButtonSubmit} 
-      /> 
-      <FaceRecognition imageUrl={this.state.imageUrl} />
-           
-    </div>
-  );
- }
+      <div className="App">
+         <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (
+             route === 'signin'
+             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
